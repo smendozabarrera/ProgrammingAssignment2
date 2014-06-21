c1d70c735fad01a0368a90d7043029bb3c741591
git <smendoza.barrera@gmail.com>
2014-06-20 22:39:26 -0500

    Testing with github site

--- src/cachematrix.R
+++ src/cachematrix.R
@@ -42,6 +42,7 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         print("Test")
+        print("Magit test 2")
 }
 
 

28b77940ead0aee989ce8d83038d20b2dbf20301
git <smendoza.barrera@gmail.com>
2014-06-20 21:58:28 -0500

    First behavior included
    Fri Jun 20 21:59:04 CDT 2014

--- src/cachematrix.R
+++ src/cachematrix.R
@@ -18,7 +18,17 @@
 ## cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m<-NULL
+        set<-function(y){
+                x<<-y
+                m<<-NULL
+        }
+        get<-function() x
+        setmatrix<-function(solve) m<<- solve
+        getmatrix<-function() m
+        list(set=set, get=get,
+             setmatrix=setmatrix,
+             getmatrix=getmatrix)
 }
 
 

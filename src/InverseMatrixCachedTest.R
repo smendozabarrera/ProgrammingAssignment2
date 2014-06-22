b153d4f6236f15aa3feb39c02cd9ba1c2a1a4d3a
git <smendoza.barrera@gmail.com>
2014-06-21 20:40:05 -0500

    cachematrix calculate and cache the inverse of a matrix

---
+++ src/InverseMatrixCachedTest.R
@@ -0,0 +1,65 @@
+##
+##
+##
+## Testing the cached inverse of a matrix
+## smendoza.barrera@gmail.com
+## Sat Jun 21 17:25:52 CDT 2014
+##
+##
+
+############################################################
+## First experiment - Normal behavior of the function
+rm(list = ls())                         # Remove all workspace data
+
+## library(MASS)
+source("cachematrix.R")
+
+seq1 <- c(1, 1, 4, 0, 3, 1, 4, 4, 0)
+myM <- matrix(seq1, 3)
+myM
+
+myInverseM <- solve(myM)                # Non cached inverse matrix
+myInverseM
+class(myInverseM)
+
+myCachedM <- makeCacheMatrix(myM)
+class(myCachedM)
+
+## First time call
+myCachedInvMatrix <- cacheSolve(myCachedM)
+myCachedInvMatrix
+
+## Second time call - getting cached data
+myCachedInvMatrix <- cacheSolve(myCachedM)
+myCachedInvMatrix
+
+############################################################
+## Second experiment - debug function
+rm(list = ls())                         # Remove all workspace data
+
+## library(MASS)
+source("cachematrix.R")
+
+seq1 <- c(1, 1, 4, 0, 3, 1, 4, 4, 0)
+myM <- matrix(seq1, 3)
+myM
+
+myInverseM <- solve(myM)                # Non cached inverse matrix
+myInverseM
+class(myInverseM)
+
+debug(makeCacheMatrix)
+myCachedM <- makeCacheMatrix(myM)
+class(myCachedM)
+
+## First time call
+debug(cacheSolve)
+myCachedInvMatrix <- cacheSolve(myCachedM)
+myCachedInvMatrix
+
+## Second time call - getting cached data
+debug(cacheSolve)
+myCachedInvMatrix <- cacheSolve(myCachedM)
+myCachedInvMatrix
+
+## EOF
--- src/cachematrix.R
+++ src/cachematrix.R
@@ -17,18 +17,29 @@
 ## makeCacheMatrix: This function creates a special "matrix" object that can
 ## cache its inverse.
 
+## The operators <<- and ->> are normally only used in functions, and cause a
+## search to made through parent environments for an existing definition of the
+## variable being assigned.
+
 makeCacheMatrix <- function(x = matrix()) {
-        m<-NULL
-        set<-function(y){
-                x<<-y
-                m<<-NULL
+        inv  <- NULL                    # Init inverse matrix symbol
+        set  <- function(y){            # Init set function to initial values
+                x <<- y                 # Input assign to x in the parent environment scope
+                inv <<- NULL            # NULL assign to inv in the parent environment scope
         }
-        get<-function() x
-        setmatrix<-function(solve) m<<- solve
-        getmatrix<-function() m
-        list(set=set, get=get,
-             setmatrix=setmatrix,
-             getmatrix=getmatrix)
+        get  <- function() x            # Init get function to x matrix
+
+
+        ## inv in this case is just an argument that is passed to the function, not
+        ## calculated, and the only thing that this function does is what's after the
+        ## parenthesis which is basically assigning the value of that argument (solve) to
+        ## a variable inv that is located in the parent function (the function inside of
+        ## which setinverse is defined).
+        setinverse  <- function(solve) inv  <<- solve # Passing the solve function to setinverse
+        getinverse  <- function() inv                 # Getting inv value from the parent environment
+        list(set= set, get = get,                     # List of functions
+             setinverse = setinverse, 
+             getinverse = getinverse)
 }
 
 
@@ -41,9 +52,16 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-        print("Test")
-        print("Magit test 2")
+        inv  <- x$getinverse()          # Calling the getinverse() of the x special matrix object
+        if (!is.null(inv)){             # If inv is not null then return cached inv value
+                message("getting cached data")
+                return(inv)
+        }
+        data  <- x$get()                # Extract the matrix class from the x special matrix defined
+                                        # in makeCacheMatrix() function
+        inv  <- solve(data, ...)        # Calling solve to calculate inverse matrix
+        x$setinverse(inv)               # Setting to x special object the inverse of matrix
+        inv                             # Return inv
 }
-
 
 ## EOF
